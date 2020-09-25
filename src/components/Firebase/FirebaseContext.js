a6c10134719b18e87c679f63aaf7dfe58424a9b4
orama254 <orama209@gmail.com>
2020-09-25 06:57:46 +0300

    Initial commit for octoquizify

---
+++ src/components/Firebase/FirebaseContext.js
@@ -0,0 +1,6 @@
+import React, {useContext} from 'react';
+
+
+export const FirebaseContext = React.createContext(null);
+export const useFirebase = () => useContext(FirebaseContext);
+
---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function Home() {
+    return (
+        <div className="container">
+            <h1>Quiz App</h1>
+            <Link to="/game" className="btn">Start Game</Link>
+            <Link to="/highScores" className="btn">High Score</Link>
+        </div>
+    )
+}
